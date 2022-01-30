2ab5278e8a4f3c4de0fdd538522030a3d4518f93
CuriousMagpie <eilatan@gmail.com>
2022-01-30 17:29:51 -0500

    lessons 13 & 14 and trying to get linting working with atom (it's not)

--- playground/custom-functions/myCf.js
+++ playground/custom-functions/myCf.js
@@ -1,10 +1,36 @@
 // function definition
-function calculateBill(){
-  //this is the function body
-  console.log('running calculateBill!');
-  const total = 100 * 1.13;
+function calculateBill(billAmount, taxRate = 0.0825, tipRate = 0.15) {
+  // this is the function body
+  const total = billAmount + billAmount * taxRate + billAmount * tipRate;
   return total;
 }
+// function call or **Run**
+natalieMeal = 250;
+delawareTax = 0.0;
+myTip = 0.2;
+const myTotal = calculateBill(natalieMeal, delawareTax, myTip);
+console.log(myTotal);
+const myTotal2 = calculateBill(1000, 0.11)
+const myTotal3 = calculateBill(100, undefined, 0.2);
 
-// this is a function call or **Run**
-calculateBill();
+// function definition
+function sayHiTo(firstName) {
+  return `Hello ${firstName}!`;
+}
+
+const greeting = sayHiTo("Natalie");
+console.log(greeting);
+
+// function definition
+function doctorize(name) {
+  return `Dr. ${name}`;
+}
+
+function yell(name = "Silly Goose") {
+  // this is a default value
+  return `HEY ${name.toUpperCase()}`;
+}
+
+const name = "Natalie";
+const yellDoctor = yell(doctorize(name));
+console.log(yellDoctor);
