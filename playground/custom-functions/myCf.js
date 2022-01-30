2803fb74697d060c9d40b5411acbbd4938daf211
CuriousMagpie <eilatan@gmail.com>
2022-01-29 23:02:53 -0500

    lesson 12 & 13 in progress

---
+++ playground/custom-functions/myCf.js
@@ -0,0 +1,10 @@
+// function definition
+function calculateBill(){
+  //this is the function body
+  console.log('running calculateBill!');
+  const total = 100 * 1.13;
+  return total;
+}
+
+// this is a function call or **Run**
+calculateBill();
