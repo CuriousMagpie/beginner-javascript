8a44e29eb60c0774fb68fa5440a5a0ebf99f621c
CuriousMagpie <eilatan@gmail.com>
2022-02-25 23:00:03 -0500

    lessons 18 & 19 - hoisting & closures

---
+++ playground/myHoisting.js
@@ -0,0 +1,15 @@
+console.log(age);
+var age = 10;
+
+// what does this file do?
+sayHi();
+
+// how does it do it?
+function sayHi() {
+  console.log('hey!');
+  console.log(add(10,2));
+}
+
+function add(a, b) {
+  return a + b
+}
