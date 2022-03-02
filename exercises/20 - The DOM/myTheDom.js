07cac23d1a7f71b2648e078f1f98c9978bdf565b
CuriousMagpie <eilatan@gmail.com>
2022-03-01 23:42:43 -0500

    lessons 21 & 22

---
+++ exercises/20 - The DOM/myTheDom.js
@@ -0,0 +1,23 @@
+// const p = document.querySelector('p');
+// const imgs = document.querySelectorAll('.items img');
+// // const item2 = document.querySelector('.item2');
+// // const item2Image = item2.querySelector('img');
+// const heading = document.querySelector('h2');
+// console.log(heading);
+// console.log(heading.innerText); // textContent used as a getter
+// console.log(heading.textContent); // supposed to show thie hidden h3 element but doesn't?
+// // heading.textContent = `One thing I'll say for him, Jesus is cool`; // textContent used as a setter
+// // console.log(heading.textContent); // textContent used as a getter
+//
+// console.log(heading.innerHTML);
+// console.log(heading.outerHTML);
+//
+// const pizzaList = document.querySelector('.pizza');
+// console.log(pizzaList.textContent);
+// // pizzaList.textContent = `${pizzaList.textContent}` + '🍕';
+// pizzaList.insertAdjacentText('afterbegin', '🍕');
+// pizzaList.insertAdjacentText('beforeend', '🍕');
+
+//classes
+const pic = document.querySelector('.nice');
+console.log(pic.classList);
